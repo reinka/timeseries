499441299c82edfd8a1175f6d767732cb6c1d5ba
reinka <andrei.poehlmann90@gmail.com>
2017-09-23 21:01:40 +0200

    [ADD] Initial commit.

---
+++ fraunhofer/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='fraunhofer',
+	description='Custom scripts for time series analysis',
+	author='Andrei Poehlmann',
+	author_email='andrei.poehlmann90@gmail.com',
+	license='MIT',
+	packages=['fraunhofer'],
+	zip_safe=False,
+)
