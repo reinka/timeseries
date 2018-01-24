fac0877381beff44053556881d2a615cd68972b6
reinka <andrei.poehlmann90@gmail.com>
2018-01-24 21:39:56 +0100

    [ADD] Updated dependencies.

--- fraunhofer/setup.py
+++ fraunhofer/setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
+
 setup(
-	name='fraunhofer',
-	description='Custom scripts for time series analysis',
-	author='Andrei Poehlmann',
-	author_email='andrei.poehlmann90@gmail.com',
-	license='AGPL-3.0',
-	packages=['fraunhofer'],
-	zip_safe=False,
+    name='fraunhofer',
+    description='Custom scripts for time series analysis',
+    author='Andrei Poehlmann',
+    author_email='andrei.poehlmann90@gmail.com',
+    license='AGPL-3.0',
+    install_requires=['pandas',
+                      'scikit-learn',
+                      'matplotlib',
+                      'seaborn',
+                      'tensorflow',
+                      'keras'
+                      ],
+    packages=['fraunhofer'],
+    zip_safe=False,
 )
